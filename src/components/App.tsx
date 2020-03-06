092b20cdf7bb2e37118ccd07fc9b1baeba917565
Will Gikandi <kawess@gmail.com>
2020-03-07 01:11:18 +1100

    Added baseline css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import Pages from './Pages';
 
 class App extends React.Component {
     render() {
         return (
-                <Pages />
+            <React.Fragment>
+                <CssBaseline />
+                {<Pages />}
+            </React.Fragment>
         );
     }
 }
