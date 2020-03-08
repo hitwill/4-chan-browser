39e97fd898a89110bfe5976193fe7cf89f07abb6
Will Gikandi <kawess@gmail.com>
2020-03-08 20:07:19 +1100

    Layout

--- src/components/FollowButton.tsx
+++ src/components/FollowButton.tsx
@@ -3,7 +3,6 @@ import Chip from '@material-ui/core/Chip';
 import Toast from './Toast';
 import Snackbar from '@material-ui/core/Snackbar';
 import MuiAlert from '@material-ui/lab/Alert';
-import { makeStyles } from '@material-ui/core/styles';
 
 function Alert(props: any) {
     return <MuiAlert elevation={6} variant="filled" {...props} />;
@@ -21,6 +20,7 @@ interface FollowState {
 class FollowButton extends React.Component<FollowProps, FollowState> {
     constructor(props: Readonly<FollowProps>) {
         super(props);
+
         this.state = {
             id: this.props.id,
             isFollowed: this.userIsFollowed(this.props.id)
@@ -35,7 +35,7 @@ class FollowButton extends React.Component<FollowProps, FollowState> {
     handleFollow(id: string) {
         let isFollowed: boolean = !this.state.isFollowed;
         this.setState({ isFollowed: isFollowed });
-        if(isFollowed){
+        if (isFollowed) {
             this.justfollowed = true;
         } else {
             this.justfollowed = false;
